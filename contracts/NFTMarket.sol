aa400b6636eac6a3a1820583b03ed92dca4c5a3a
Tayfun <martayfun@gmail.com>
2022-06-13 19:30:11 +0300

    update sol development

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -1,20 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract NFT is ERC721URIStorage {
+contract NFTMarket is ReentrancyGuard{
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    address contractAddress;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
 
-    constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
-        contractAddress = marketplaceAddress;
+    address payable owner;
+    uint256 listingPrice = 0.025 ether;
+
+    constructor(){
+        owner = payable(msg.sender);
     }
 
-    function createToken(string memory tokenURI) public returns (uint) {
-        
+    struct MarketItem {
+        uint itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    event MarketItemCreated (
+        uint indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function createMarketItem (
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant {
+        require(price > 0, "Price must be at least 1 wei.");
+        require(msg.value == listingPrice, "Price must be equal to listing price.");
+
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+
+        idToMarketItem[itemId] = MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
     }
 }
