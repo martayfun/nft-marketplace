21ceae0191a82fc6174a9bbcc8bcae2540ae8f6d
Tayfun Ates <tayfun.ates@odeontechnology.com>
2022-06-15 14:14:50 +0300

    update solidity development

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,19 +3,28 @@ const { ethers } = require("hardhat");
 
 describe("NFTMarket", function () {
   it("Should create and execute market sales", async function () {
-    const marketFactory = await ethers.getContractFactory("NFTMarket");
-    const market = await marketFactory.deploy();
-    await market.deployed();
+    const marketFactory = await ethers.getContractFactory("NFTMarket")
+    const market = await marketFactory.deploy()
+    await market.deployed()
     const marketAddress = market.address
 
-    const nftFactory = await ethers.getContractFactory("NFT");
-    const nft = await nftFactory.deploy(marketAddress);
-    await nft.deployed();
-    const nftContractAddress = nft.address;
+    const nftFactory = await ethers.getContractFactory("NFT")
+    const nft = await nftFactory.deploy(marketAddress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
 
-    let listingPrice = await market.getListingPrice();
-    listingPrice = listingPrice.toString();
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
 
-    const auctionPrice = ethers.utils.parseUnits('100', 'ether');
+    const auctionPrice = ethers.utils.parseUnits('100', 'ether')
+
+    await nft.createToken("https://www.mytokenlocation.com")
+    await nft.createToken("https://www.mytokenlocation2.com")
+
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
+
+    const [_, buyerAddress] = await ethers.getSigner()
   });
 });
+ 
